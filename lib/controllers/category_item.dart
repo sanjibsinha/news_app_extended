4df6ac5c4204e0feae05aba9865121176ad6d99c
Sanjib <sanjib12sinha@gmail.com>
2021-08-26 12:27:56 +0530

    category container modified

--- lib/controllers/category_item.dart
+++ lib/controllers/category_item.dart
@@ -34,7 +34,7 @@ class CategoryItem extends StatelessWidget {
           title,
           style: Theme.of(context).textTheme.headline6,
         ),
-        decoration: BoxDecoration(
+        /* decoration: BoxDecoration(
           gradient: LinearGradient(
             colors: [
               color.withOpacity(0.7),
@@ -44,6 +44,38 @@ class CategoryItem extends StatelessWidget {
             end: Alignment.bottomRight,
           ),
           borderRadius: BorderRadius.circular(15),
+        ), */
+        alignment: Alignment.center,
+//color: Colors.blue,
+// we can adjust width and height
+// to do that we need to commented out the constraints
+        width: 350.00,
+        height: 350.00,
+// to skew the container
+        transform: Matrix4.rotationZ(0.1),
+        decoration: BoxDecoration(
+          color: Colors.blue,
+          border: Border.all(
+            color: Colors.red,
+            width: 2.0,
+            style: BorderStyle.solid,
+          ),
+          borderRadius: const BorderRadius.all(Radius.circular(40.0)),
+          boxShadow: const [
+            BoxShadow(
+              color: Colors.black54,
+              blurRadius: 20.0,
+              spreadRadius: 20.0,
+            ),
+          ],
+          gradient: const LinearGradient(
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight,
+            colors: [
+              Colors.red,
+              Colors.white,
+            ],
+          ),
         ),
       ),
     );
